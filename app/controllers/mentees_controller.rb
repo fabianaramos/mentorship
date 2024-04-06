6b39e9dc554ef3ae69829600a34aab717e606f0b
helenice Silva <helenice@inteli.fi>
2024-04-06 17:18:39 -0300

    wip: tela create mentees

--- app/controllers/mentees_controller.rb
+++ app/controllers/mentees_controller.rb
@@ -3,20 +3,34 @@ class MenteesController < ApplicationController
     @mentees = Mentee.all
   end
 
-  def shows(id)
-    @mentees = Mentee.find(id)
+  def show
+    @mentee = Mentee.find(params[:id])
   end
 
   def new
-    
+    p current_user
+    @mentee = Mentee.new
   end
 
   def create
+    p mentee_params
+    @mentee = Mentee.new(mentee_params)
+
+    if @mentee.save
+      redirect_to @mentee
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def mentee_params
+    params.require(:mentee).permit(:bio).merge(user_id: current_user.id)
   end
 end
